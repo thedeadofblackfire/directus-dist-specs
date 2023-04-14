5681cf486a132961b76c674fe0e946b1c4938d8c
Nicolas Thomas <thedeadofblackfire@yahoo.com>
2023-04-14 12:00:10 +0200

    fix

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { readFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+//console.log(dirname(fileURLToPath(import.meta.url)));
+//console.log(require.resolve('./dist/openapi.json')); 
+	
 export const spec = JSON.parse(
-	console.log(dirname(fileURLToPath(import.meta.url)));
-	console.log(require.resolve('./dist/openapi.json')); 
 	readFileSync(resolve('./dist/openapi.json'), 'utf8');
 	//readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './dist/openapi.json'), 'utf8')
 );
